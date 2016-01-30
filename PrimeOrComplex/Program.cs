fb8bc310f7ce4d3628e6dfb26a0fffb788180a7c
darkhan97cool <darkhan97cool>
2016-01-31 03:37:45 +0600

    PrimeOrComplex

---
+++ PrimeOrComplex/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeOrComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter input");
+            int a = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            for(int i=2;i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    cnt++;
+                }
+            }
+            if (cnt == 1)
+            {
+                Console.WriteLine("Entered number is Prime");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is Complex");
+            }           
+            Console.ReadKey();
+        }
+    }
+}

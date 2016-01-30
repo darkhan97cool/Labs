981435848aef0c13c63d61e91d40e4ad6fe90f53
darkhan97cool <darkhan97cool>
2016-01-31 03:49:03 +0600

    PrimeOrComplex

--- PrimeOrComplex/Program.cs
+++ PrimeOrComplex/Program.cs
@@ -8,20 +8,25 @@ namespace PrimeOrComplex
 {
     class Program
     {
+        /// <summary>
+        /// Here, I can enter just one number but 
+        /// I want enter set of numbers then find prime numbers from there.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Please enter input");
+            Console.WriteLine("Please enter input...");
             int a = int.Parse(Console.ReadLine());
             int cnt = 0;
-            for(int i=2;i <= a; i++)
+            for(int i=1;i <= a; i++)
             {
                 if (a % i == 0)
                 {
                     cnt++;
                 }
             }
-            if (cnt == 1)
+            if (cnt == 2)
             {
                 Console.WriteLine("Entered number is Prime");
             }
